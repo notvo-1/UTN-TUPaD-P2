ce5967e9f6679d94eb1d6b68e439036be2f0c4b7
Notvo <friedrice1918@gmail.com>
2025-10-24 22:05:01 -0300

    TP8 Interfaces y Excepciones

--- 08 Interfaces y Excepciones/TrabajoPractico8/src/Parte2/Ejercicio.java
+++ 08 Interfaces y Excepciones/TrabajoPractico8/src/Parte2/Ejercicio.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package Parte2.Ejercicio1;
+package Parte2;
 
 import com.sun.source.tree.TryTree;
 import java.io.BufferedReader;
--- 08 Interfaces y Excepciones/TrabajoPractico8/src/Parte2/Principal.java
+++ 08 Interfaces y Excepciones/TrabajoPractico8/src/Parte2/Principal.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-package Parte2.Ejercicio1;
+package Parte2;
 
 import java.util.Scanner;
 
